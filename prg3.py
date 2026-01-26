d66056911ab043c0cfda48e9a3975d2529658ed2
Poornima G R <naganaga.p7288@gmail.com>
2026-01-26 21:53:53 +0530

    Add files via upload

---
+++ prg3.py
@@ -0,0 +1,47 @@
+'''num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+if num1>=num2:
+    print("this is watch:",num1)
+else:
+    print("this is pen:",num2)'''
+    
+'''num1=int(input("enter the first number:"))
+square= num1**2
+print(square)'''
+
+
+'''num1=int(input("entr the length:"))
+num2=int(input("enter the breadth:"))
+rect=num1*num2
+print("The area of the rectangle is:",rect)'''
+
+
+'''celcius=float(input("enter the celcius:"))
+fahr=(celcius*9/5)+32
+print("The Fahrenheit is :",fahr)'''
+
+
+'''salary=float(input("enter the salary:"))
+bonus=salary*0.10
+result=salary+bonus
+print("The slary bonus is:",bonus)
+print("the total amount is:",result)'''
+
+
+'''salary = float(input("Enter your salary: "))
+bonus = salary * 0.10
+total_salary = salary + bonus
+
+print("Bonus amount:", bonus)
+print("Total salary after bonus:", total_salary)'''
+
+
+num1=int(input("enter the number:"))
+if num1>0:
+    print("the number is positive:")
+elif num1<0:
+    print("the numeber is negative:")
+else:
+    print("the numbe is zero:")
+    
+    

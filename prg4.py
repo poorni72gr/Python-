a828290cd2470c3f698fcb0f547d616cfdcc90a1
Poornima G R <naganaga.p7288@gmail.com>
2026-01-27 11:14:08 +0530

    Add files via upload

---
+++ prg4.py
@@ -0,0 +1,13 @@
+marks = int(input("enter your marks :"))
+if marks <35:
+    print("fail")
+elif marks >=35 and marks <50:
+    print("c grade")
+elif marks >=50 and marks <70:
+    print("b grade")
+elif marks >=70 and marks <80:
+    print("a grade")
+elif marks >=80 and marks <100:
+    print("a+ grade")
+else:
+      print("invalid marks")

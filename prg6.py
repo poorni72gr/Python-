ac23327fe79fd98cbbcb62b81d8497706fda5459
Poornima G R <naganaga.p7288@gmail.com>
2026-01-27 11:16:25 +0530

    Add files via upload

---
+++ prg6.py
@@ -0,0 +1,7 @@
+ect = int(input( "enter your electricity unit"))
+if ect<100:
+    print(" the cost is 1 rupee")
+elif ect>100 and ect<200:
+    print(" the cost is 2 rupee")
+else:
+    print(" the cost is 5 rupee")

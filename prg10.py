9a92e18966ba8f780473d2faa6f40428d2c901ae
Poornima G R <naganaga.p7288@gmail.com>
2026-02-09 16:46:25 +0530

    Add files via upload

---
+++ prg10.py
@@ -0,0 +1,39 @@
+'''lst=[10,20,30,40,50]
+print(lst)'''
+
+'''lst=[12,2.3,True,(2+4j),"python"]
+print(lst)'''
+
+'''lst=[10,[10,20,30],(50,60,70),{2,3,4},{1:'x',2:'y'}]
+print(lst)
+print(lst[0])
+print(lst[1][1])
+print(lst[2])
+print(lst[3])
+print(lst[4])
+print(lst[2][2])
+print(lst[4][2])'''
+
+'''lst=[10,20,30]
+print(sum(lst))'''
+
+#sum manual function
+
+'''sum=0
+for x in [10,20,30]:
+    sum=sum+(x**2)
+print(sum)'''
+'''sum=0
+for x in [10,20,30]:
+    sum=sum+x
+print(sum)'''
+
+'''p=1
+for x in [1,2,3]:
+    p=p**1
+print(p)'''
+
+'''count=0
+for x in [1,2,3]:
+    count=count+1
+print(count)'''

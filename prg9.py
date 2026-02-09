a470498f1c74d868de2c6c452e7644b1c2410f67
Poornima G R <naganaga.p7288@gmail.com>
2026-02-09 16:46:03 +0530

    Add files via upload

---
+++ prg9.py
@@ -0,0 +1,30 @@
+'''for x in "123456":
+    print(x)'''
+
+#range function
+#start=0
+#stop=n-1
+#ends=n-1
+'''for x in range(6):
+    print(x)'''
+
+
+'''for x in range(100,105):
+    print(x)'''
+
+#(start,stop,step)
+'''for i in range(3,30,3):
+    print(i)'''
+
+'''for x in range(9,91,9):
+    print(x)'''
+
+
+#square of number
+'''for i in range(5):
+    print(i**2)'''
+
+# cube of number
+for i in range(10):
+    print(i**3)
+    
